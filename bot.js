05539964493e10a75c2db3b6f5b44b124c8a5047
scotty <scdual@gmail.com>
2021-01-11 19:09:15 -0600

    brrrmmm

--- bot.js
+++ bot.js
@@ -15,8 +15,9 @@ client.on('ready', () => {
 client.on('message', message => {
     console.log(message)
     if (message.content.includes('<@!343496726302162944>')) {
-       var user = message.author.username
-       var reply = '!warn @' + user + ' do not ping owner.  Go read the rules #welcome-rules'
+       var user = message.author.id
+       var channelId = message.channel.id
+       var reply = '!warn <@' + user + '> do not ping owner.  Go read the rules <#' + channelId + '>'
        message.channel.send(reply);
     }
 });
